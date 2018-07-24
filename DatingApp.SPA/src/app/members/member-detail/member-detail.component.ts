e2e69489d09bfd41ec8e76b2c751ad3712ef8201
Vandan Panwala <vandanpanwala@gmail.com>
2018-07-24 19:42:21 +0530

    End of section 9

--- DatingApp.SPA/src/app/members/member-detail/member-detail.component.ts
+++ DatingApp.SPA/src/app/members/member-detail/member-detail.component.ts
@@ -3,6 +3,7 @@ import { User } from '../../_models/User';
 import { UserService } from '../../_services/user.service';
 import { AlertifyService } from '../../_services/alertify.service';
 import { ActivatedRoute } from '@angular/router';
+import { NgxGalleryOptions, NgxGalleryImage, NgxGalleryAnimation } from 'ngx-gallery';
 
 @Component({
   selector: 'app-member-detail',
@@ -12,6 +13,9 @@ import { ActivatedRoute } from '@angular/router';
 
 export class MemberDetailComponent implements OnInit {
   user: User;
+  galleryOptions: NgxGalleryOptions[];
+  galleryImages: NgxGalleryImage[];
+
   constructor(private userService: UserService,
     private alertify: AlertifyService,
     private route: ActivatedRoute) { }
@@ -20,7 +24,40 @@ export class MemberDetailComponent implements OnInit {
     this.route.data.subscribe(data => {
       this.user = data['user'];
     });
+
+    // TODO:
+    // set the gallery configurations by using this.gallerOptions = [{parameters}] syntax
+    this.galleryOptions = [{
+      width: '500px',
+      height: '500px',
+      imagePercent: 100,
+      thumbnailsColumns: 4,
+      imageAnimation: NgxGalleryAnimation.Slide,
+      preview: true
+    }];
+
+    this.galleryImages = this.getImages();
   }
+  // TODO:
+  // Get the gallery images as an array
+  getImages() {
+    const imageUrls = [];
+    for (let i = 0; i < this.user.photos.length; i++) {
+      imageUrls.push({
+        small: this.user.photos[i].url,
+        medium: this.user.photos[i].url,
+        big: this.user.photos[i].url,
+        description: this.user.photos[i].description
+      });
+    }
+
+    return imageUrls;
+  }
+
+
+  
+
+
   // members/3
   // this method receives the id from the member-card.component.html and then uses the
   // userService to get a particular user from the API
